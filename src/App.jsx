746da93d5407242779d93e2a101de77edfce4583
Ian Co <bythewayimian@gmail.com>
2025-08-03 21:16:19 +0100

    Set up Spotify Implicit Grant flow and access token extraction

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import SearchBar from './SearchBar';
+import { getAccessToken } from './spotify';
 
 const App = () => {
+  useEffect(() => {
+    const token = getAccessToken();
+    if (token) {
+      console.log('Access Token:', token);
+    } else {
+      console.log('No access token found');
+    }
+  }, []);
+
   return (
     <SearchBar />
   )
--- src/Spotify.js
+++ src/Spotify.js
@@ -1,6 +1,18 @@
 const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
 const redirectUri = import.meta.env.VITE_SPOTIFY_REDIRECT_URI;
+const authEndpoint = 'https://accounts.spotify.com/authorize';
+const scope = 'playlist-modify-public';
+const authUrl =  `${authEndpoint}?client_id=${clientId}&response_type=token&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}`;
 
+export const getAccessToken = () => {
+    const hash = window.location.hash;
+    let accessToken = null;
+    if (hash) {
+        accessToken = hash.split('&').find(elem => elem.startsWith('access_token=')).split('=')[1];
+        window.location.hash = ''; // Clear the hash from the URL
+    }
+    return accessToken;
+}
 
 const searchSpotify = (searchTerm) => {
     // This function will fetch search results from the Spotify API
