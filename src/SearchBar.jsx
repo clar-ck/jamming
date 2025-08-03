d85c937be8d2e318e58d1fb76176fc2e7cbd46ab
Ian Co <bythewayimian@gmail.com>
2025-08-03 17:03:25 +0100

    Add state and onChange handler to SearchBar input

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchBar = () => {
+    const [Search, setSearch] = useState('');
+
+    const handleChange = ({target}) => {
+        setSearch(target.value);
+    }
+
     return (
         <>
         <div>Search Bar</div>
-        <input type="text" placeholder="Search..." />
+        <input type="text" value={Search} onChange={handleChange} placeholder="Search..." />
         <button>Search</button>
         <button>Clear</button>
         </>
