ef2e3d01ed8e4c6db3bb11adc89dea21930eb78c
Ian Co <bythewayimian@gmail.com>
2025-08-03 18:55:11 +0100

    Add .env file and update Spotify credentials usage

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,3 +1,7 @@
+const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+const redirectUri = import.meta.env.VITE_SPOTIFY_REDIRECT_URI;
+
+
 const searchSpotify = (searchTerm) => {
     // This function will fetch search results from the Spotify API
     alert(`Searching Spotify for: ${searchTerm}`);
