0ebc171d5acce83c053921951cebb6daa2989599
Ian Co <bythewayimian@gmail.com>
2025-08-05 22:47:48 +0100

    style: add track list and track item styling

--- src/Playlist.jsx
+++ src/Playlist.jsx
@@ -59,9 +59,9 @@ const Playlist = ({ playlist, onNameChange, onRemoveTrack, accessToken }) => {
             <h3>Tracks</h3>
             {playlist.tracks.length === 0 && <p>No tracks in the playlist</p>}
 
-            <ul>
+            <ul className="track-list">
                 {playlist.tracks.map(track => (
-                    <li key={track.id}>
+                    <li className="track-item" key={track.id}>
                         {track.name} by {track.artists[0]?.name || 'Unknown Artist'}
                         <button onClick={() => onRemoveTrack(track.id)}>Remove</button>
                     </li>
